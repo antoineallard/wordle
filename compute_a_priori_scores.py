b3e95ac51c66562724078e92ec72f1be21fc88ea
antoineallard <antoine.allard.1@gmail.com>
2022-01-20 07:23:58 -0500

    today's puzzle

--- compute_a_priori_scores.py
+++ compute_a_priori_scores.py
@@ -5,16 +5,16 @@
 # The execution of this code is quite long (each ordered pair of word must be considered).
 
 import pandas
+import numpy
 from wordle_solver import wordle_solver
 
 
 ws = wordle_solver()
-df = ws.get_compatible_word_scores()
-
+df = ws.get_compatible_word_scores().reset_index()
 with open('data/a_priori_scores.txt', 'w') as ofile:
 
     header = '# {:4s} {:>8s} {:>8s}\n'.format('Word', 'SumScore', 'AvgScore')
     ofile.write(header)
 
     fmt_str = '%6s %8d %8.3f'
-    np.savetxt(ofile, df.values, fmt=fmt_str)
+    numpy.savetxt(ofile, df.values, fmt=fmt_str)
